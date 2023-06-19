7f2216d2623cabe1f2e1082df9e5b9c908e58bd1
k0Iry <kljsandjb@me.com>
2023-06-19 21:43:11 +0100

    Initial Commit

---
+++ Invader/InvaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvaderApp.swift
+//  Invader
+//
+//  Created by xintu on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct InvaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
